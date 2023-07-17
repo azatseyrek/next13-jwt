3dcaf79201dd170974feefac293569e0cabd0afa
Azat Seyrek <azat.seyrek@adesso.com.tr>
2023-07-17 16:34:26 +0300

    env name updated

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -1,7 +1,7 @@
 import { jwtVerify } from 'jose';
 
 export function getJwtSecretKey() {
-  const secret = process.env.JWT_SECRET_KEY;
+  const secret = process.env.NEXT_PUBLIC_JWT_SECRET_KEY;
 
   if (!secret) {
     throw new Error('JWT Secret key is not matched');
